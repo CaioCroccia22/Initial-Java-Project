e0b5a1afd4a0f1fa44a4208a20397c54df3625bb
caiocroccia22 <caiocroccia22@gmail.com>
2026-01-04 22:34:13 -0300

    feat: implement cliente, funcionario, itemVenda class

---
+++ Users/Caio/eclipse-workspace/Initial_Project/src/br/com/initial/project/Funcionario.java
@@ -0,0 +1,7 @@
+package br.com.initial.project;
+
+public class Funcionario {
+		private int codigo;
+		private String nome;
+		private String endereco;
+}
---
+++ Users/Caio/eclipse-workspace/Initial_Project/src/br/com/initial/project/ItemVenda.java
@@ -0,0 +1,5 @@
+package br.com.initial.project;
+
+public class ItemVenda {
+
+}
